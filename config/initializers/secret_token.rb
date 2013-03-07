c891dc84b66ef624f4b3d56a5a6df250655bb689
nickkevin <nickkevin.35@gmail.com>
2013-03-07 18:23:40 +0530

    break the band

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Breakme::Application.config.secret_token = '2f4c8f85a00b1141cfbdfc1c82f7011508dc38ce023f47512582001ea13b1c1ba1fc8560766ed46c1488edd91f80a717a9a8eb7f9b3947c3897f4a795461ee02'
